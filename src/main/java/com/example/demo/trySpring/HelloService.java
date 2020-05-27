ce7cbd76288936f064bfee31162ee6b9d0313d9e
honda0924 <honda0924@hotmail.co.jp>
2020-05-27 23:54:07 +0900

    fix_bugs

--- src/main/java/com/example/demo/trySpring/HelloService.java
+++ src/main/java/com/example/demo/trySpring/HelloService.java
@@ -6,21 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class HelloService{
-	@Autowired
-	private HelloRepository helloRepository;
+public class HelloService {
 
-	public Employee findOne(int id) {
-		Map<String,Object>map = helloRepository.findOne(id);
+    @Autowired
+    private HelloRepository helloRepository;
 
-		int employeeId=(Integer)map.get("employee_id");
-		String employeeName = (String)map.get("employee_name");
-		int age = (Integer)map.get("age");
-		Employee employee = new Employee();
-		employee.setEmployeeId(employeeId);
-		employee.setEmployeeName(employeeName);
-		employee.setAge(age);
+    public Employee findOne(int id) {
 
-		return employee;
-	}
+        // １件検索実行
+        Map<String, Object> map = helloRepository.findOne(id);
+
+        // Mapから値を取得
+        int employeeId = (Integer) map.get("employee_id");
+        String employeeName = (String) map.get("employee_name");
+        int age = (Integer) map.get("age");
+
+        // Employeeクラスに値をセット
+        Employee employee = new Employee();
+        employee.setEmployeeId(employeeId);
+        employee.setEmployeeName(employeeName);
+        employee.setAge(age);
+
+        return employee;
+    }
 }
