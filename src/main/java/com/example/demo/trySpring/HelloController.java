b2b7d562a0dd7c24dc5445ace0c6330acf62c018
honda0924 <honda0924@hotmail.co.jp>
2020-05-27 22:07:22 +0900

    search

--- src/main/java/com/example/demo/trySpring/HelloController.java
+++ src/main/java/com/example/demo/trySpring/HelloController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController{
+	@AutoWired
+	private HelloService helloService;
+	
 	@GetMapping("/hello")
 	public String getHello() {
 		return "hello";
@@ -14,10 +17,24 @@ public class HelloController{
 	@PostMapping("/hello")
 	public String postRequest(@RequestParam("text1")String str,Model model) {
 		model.addAttribute("sample", str);
-		
+
 		return "helloResponse";
 	}
-	
-	
+	@PostMapping("/hello/db")
+	public String postDbRequest(@RequestParam("text2")String str,Model model) {
+
+		int id=Integer.parseInt(str);
+
+		Employee employee=helloService.findOne(id);
+		
+		model.addAttribute("id",employee.getEmployeeId());
+		model.addAttribute("name",employee.getEmployeeName());
+		model.addAttribute("age",employee.getAge());
+		
+		return "helloResponseDB";
+		
+	}
+
+
 }
 
