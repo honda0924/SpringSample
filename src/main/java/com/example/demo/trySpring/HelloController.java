59773cb2e5be29e060348c1f237b3872de2d188d
honda0924 <honda0924@hotmail.co.jp>
2020-05-27 22:49:48 +0900

    fix_controller

--- src/main/java/com/example/demo/trySpring/HelloController.java
+++ src/main/java/com/example/demo/trySpring/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.trySpring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController{
-	@AutoWired
+	@Autowired
 	private HelloService helloService;
 	
 	@GetMapping("/hello")
